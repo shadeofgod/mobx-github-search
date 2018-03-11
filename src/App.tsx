eeb174f0e4e93b2044bc59c6b1bc805ea029937e
zoubingwu <zoubingwu@gmail.com>
2018-03-12 01:05:27 +0800

    add root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import * as React from 'react'
+import { observer, inject } from 'mobx-react'
+import DevTools from 'mobx-react-devtools'
 
-export class App extends React.Component {
+import Input from './components/Input'
+import List from './components/List'
+
+import { ListStore } from './store/ListStore'
+import { ViewStore } from './store/ViewStore'
+
+export interface IProps {
+  listStore: ListStore
+  viewStore: ViewStore
+}
+
+@inject('listStore')
+@inject('viewStore')
+@observer
+export class App extends React.Component<{}, {}> {
   render() {
-    return <div>Hello world!</div>
+    const { viewStore, listStore } = this.props as IProps
+
+    return (
+      <div>
+        <Input listStore={listStore}/>
+        <List isLoading={viewStore.isLoading} listStore={listStore}/>
+        {process.env.NODE_ENV !== 'production' && <DevTools />}
+      </div>
+    )
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { useStrict } from 'mobx'
+import { Provider } from 'mobx-react'
 
+import listStore from './store/ListStore'
+import viewStore from './store/ViewStore'
 import { App } from './App'
 
+useStrict(true)
+
+const stores = { listStore, viewStore }
+
 ReactDOM.render(
-  <App />,
+  <Provider {...stores}>
+    <App />
+  </Provider>,
   document.getElementById('app') as HTMLElement,
 )
